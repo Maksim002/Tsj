e56dbb634addc4a8a00ee17afbf7dd7706632f78
doseeare <k7karimov@gmail.com>
2020-02-21 16:35:58 +0600

    #29031

--- app/src/main/java/com/example/tsj/adapters/save/SaveListener.kt
+++ app/src/main/java/com/example/tsj/adapters/save/SaveListener.kt
@@ -3,6 +3,5 @@ package com.example.tsj.adapters.save
 import com.example.tsj.model.SaveModel
 
 interface SaveListener {
-
     fun onClick(save: SaveModel)
 }
--- app/src/main/java/com/example/tsj/ui/balance/BalanceFragment.kt
+++ app/src/main/java/com/example/tsj/ui/balance/BalanceFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import com.example.tsj.R
 import kotlinx.android.synthetic.main.fragment_balance.*
@@ -25,6 +26,8 @@ class BalanceFragment : Fragment() {
         showBalanceButton.setOnClickListener {
           findNavController().navigate(R.id.navigation_balance_detail)
         }
+        (activity as AppCompatActivity).supportActionBar?.show()
+
         return root
     }
 }
