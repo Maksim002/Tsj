a9385fc2a599bdc936117b014dc1c17455ac4098
User <Maksim002>
2020-03-03 16:41:54 +0600

    #29471

---
+++ app/src/main/java/com/example/tsj/service/model/BalanceStatusModel.kt
@@ -0,0 +1,14 @@
+package com.example.tsj.service.model
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class BalanceStatusModel {
+
+    @SerializedName("ServiceName")
+    @Expose
+    var serviceName: String? = null
+    @SerializedName("Balance")
+    @Expose
+    var balance: Double? = null
+}
---
+++ app/src/main/java/com/example/tsj/ui/balance/BalanceDetailViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.tsj.ui.balance
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.tsj.service.RetrofitService
+import com.example.tsj.service.model.BalanceStatusModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class BalanceDetailViewModel : ViewModel() {
+
+    fun services(placementId: Int): LiveData<List<BalanceStatusModel>> {
+        val data = MutableLiveData<List<BalanceStatusModel>>()
+
+
+        RetrofitService.apiServise().services(placementId)
+            .enqueue(object : Callback<List<BalanceStatusModel>> {
+                override fun onResponse(
+                    call: Call<List<BalanceStatusModel>>,
+                    response: Response<List<BalanceStatusModel>>
+                ) {
+                    data.value = response.body()
+                println()
+                }
+
+                override fun onFailure(call: Call<List<BalanceStatusModel>>, t: Throwable) {
+
+                }
+            })
+
+
+        return data
+    }
+}
