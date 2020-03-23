092a9548795d60490e2ac8ff6ebe305dad3d51a6
b.gasanov <b.g.12qw!@QW>
2020-03-23 13:21:21 +0600

    #30763

--- app/src/main/java/com/example/tsj/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/tsj/ui/login/LoginActivity.kt
@@ -71,7 +71,7 @@ class LoginActivity : AppCompatActivity() {
 
     private fun validate(): Boolean{
         var valid = true
-        if (text_email.text.toString().isEmpty() || MyUtils.emailValidate(text_email.text.toString())) {
+        if (text_email.text.toString().isEmpty() || !MyUtils.emailValidate(text_email.text.toString())) {
             main_container_email_input.error = "Введите правильный адрес"
             valid = false
         }else{
@@ -120,13 +120,6 @@ class LoginActivity : AppCompatActivity() {
         return password.isNotEmpty()
     }
 
-    //проверка email
-    private fun validateEmail(email: String): Boolean {
-        val regExpn =
-            ("""^(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})$""")
-        val pattern = Pattern.compile(regExpn, Pattern.CASE_INSENSITIVE)
-        val matcher = pattern.matcher(email)
-        return matcher.matches()
-    }
+
 }
 
