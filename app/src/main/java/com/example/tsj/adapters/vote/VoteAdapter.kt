a0efd2259e7045e95be98f7376d6e4cd0042d9e1
b.gasanov <b.g.12qw!@QW>
2020-03-24 12:40:56 +0600

    #30923

--- app/src/main/java/com/example/tsj/adapters/vote/VoteAdapter.kt
+++ app/src/main/java/com/example/tsj/adapters/vote/VoteAdapter.kt
@@ -9,13 +9,12 @@ import com.example.tsj.common.ViewHolder
 import com.example.tsj.model.VoteModel
 import kotlinx.android.synthetic.main.item_vote.view.*
 
-class VoteAdapter(listener: VoteItemClickListener, item: List<VoteModel>) :
+class VoteAdapter(private val listener: VoteItemClickListener, item: List<VoteModel>) :
     GenericRecyclerAdapter<VoteModel>(item) {
-    private val listener: VoteItemClickListener = listener
     override fun bind(item: VoteModel, holder: ViewHolder) {
         holder.itemView.vote_content.text = item.question
-        holder.itemView.setOnClickListener { listener.onVoteItemClicked(item) }
-        holder.itemView.vote_btn.setOnClickListener { listener.onVoteButtonClicked(item, holder.adapterPosition) }
+        holder.itemView.setOnClickListener { listener.onClick(item,false) }
+        holder.itemView.vote_btn.setOnClickListener { listener.onClick(item,true) }
 
         if (!item.isCanVote) {
             holder.itemView.vote_btn.visibility = View.GONE
