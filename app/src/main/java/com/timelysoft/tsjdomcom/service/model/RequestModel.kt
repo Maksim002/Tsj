936b5872108948bb543a710d3b7c5c4350d4cc93
Maksim002 <inoplanee@mail.ru>
2020-04-29 13:47:01 +0600

    #33632 #33633

--- app/src/main/java/com/timelysoft/tsjdomcom/service/model/RequestModel.kt
+++ app/src/main/java/com/timelysoft/tsjdomcom/service/model/RequestModel.kt
@@ -13,12 +13,26 @@ class RequestModel {
     @SerializedName("Floor")
     @Expose
     var floor: Int = 0
+
+    @SerializedName("CreatedDate")
+    @Expose
+    var createdDate: String = ""
+
     @SerializedName("Id")
     @Expose
     var id: Int = 0
     @SerializedName("RequestTypeName")
     @Expose
     var requestTypeName: String = ""
+
+    @SerializedName("StatusName")
+    @Expose
+    var statusName: String = ""
+
+    @SerializedName("StatusDate")
+    @Expose
+    var statusDate: String = ""
+
     @SerializedName("IsEditableAndCloseable")
     @Expose
     var editableAndCloseable: Boolean = false
