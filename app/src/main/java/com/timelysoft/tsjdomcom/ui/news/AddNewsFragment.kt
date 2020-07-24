8600a91573df1462d13688e0e577bf916f1ab811
Maksim002 <inoplanee@Mail.ru>
2020-07-24 17:19:07 +0600

    #39433

--- app/src/main/java/com/timelysoft/tsjdomcom/ui/news/AddNewsFragment.kt
+++ app/src/main/java/com/timelysoft/tsjdomcom/ui/news/AddNewsFragment.kt
@@ -1,23 +1,20 @@
 package com.timelysoft.tsjdomcom.ui.news
 
-import android.database.Cursor
-import android.net.Uri
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
-import android.provider.MediaStore.MediaColumns
 import android.view.*
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.loader.content.CursorLoader
 import com.timelysoft.tsjdomcom.R
 import com.timelysoft.tsjdomcom.adapters.news.AddNewsAdapter
 import com.timelysoft.tsjdomcom.adapters.news.AddNewsModel
 import com.timelysoft.tsjdomcom.service.AppPreferences
-import com.timelysoft.tsjdomcom.ui.contact.fragments.AccountsBottomSheet
 import kotlinx.android.synthetic.main.fragment_add_news.*
-import kotlinx.android.synthetic.main.fragment_contacts.view.*
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody.Companion.asRequestBody
@@ -25,13 +22,13 @@ import java.io.File
 
 
 class AddNewsFragment : Fragment() {
-
-    private lateinit var addNewsBottomShit: AddNewsBottomShitFragment
+    private val STORAGE_PERMISION_CODE: Int = 1
+    private val IMAGE_PICK_CODE = 10
+    private var files = ArrayList<MultipartBody.Part>()
+    private var names = ArrayList<String>()
 
     private var myAdapter = AddNewsAdapter()
     var list: ArrayList<AddNewsModel> = arrayListOf()
-
-    private var position: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,24 +40,17 @@ class AddNewsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        addNewsBottomShit = AddNewsBottomShitFragment()
         setHasOptionsMenu(true)
         iniArgument()
         initRecycler()
     }
 
     private fun iniArgument() {
-        val item = try {
-            arguments!!.getSerializable("file")
-        }catch (e: Exception){
-            ""
-        }
-        list.add(AddNewsModel(position, item.toString()))
-        myAdapter.update(list)
 
     }
 
     private fun initRecycler() {
+        myAdapter.update(list)
         add_news_recycler.adapter = myAdapter
     }
 
@@ -76,9 +66,60 @@ class AddNewsFragment : Fragment() {
 
             }
             R.id.fasten_file -> {
-                addNewsBottomShit.show(fragmentManager!!, "addNewsBottomShit")
+                loadFiles()
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun loadFiles() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(context!!, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                val permissions = arrayOf(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+                requestPermissions(permissions, STORAGE_PERMISION_CODE)
+            } else {
+                getMyFile()
+            }
+        } else {
+            getMyFile()
+        }
+    }
+
+    private fun getMyFile() {
+        val myFile = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        myFile.setType("*/*");
+        startActivityForResult(Intent.createChooser(myFile,"Select Picture") , IMAGE_PICK_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            getMyFile()
+        } else {
+            Toast.makeText(context, "Нет разрешений", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+
+            if (data != null) {
+                val uri = data.data!!
+                val file = File(uri.toString())
+                val requestFile = file.asRequestBody("file/*".toMediaTypeOrNull())
+                val photo = MultipartBody.Part.createFormData("File", file.name, requestFile)
+                files.add(photo)
+                names.add(photo.toString())
+                val filename = file.name
+                list.add(AddNewsModel(0, filename))
+                myAdapter.notifyDataSetChanged()
+            }
+        }
     }
 }
